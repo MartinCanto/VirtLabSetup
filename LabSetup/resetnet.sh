2785717d8c999d7ab48d7c98ae94d1733ce0b66e
Martin <1086970216@mil>
2017-01-09 07:49:09 -0500

    fixed reset script

--- LabSetup/resetnet.sh
+++ LabSetup/resetnet.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 # Joseph Martin Dec 2016
 
-nodes=$(ls -C /LabSetup/ks | tr -s ' ' ',')
-networks=$(ls -C /LabSetup/virtnet | tr -s ' ' ',')
+nodes=$(ls -C /LabSetup/ks)
+virtnets=$(ls -C /LabSetup/virtnet)
 httpstatus=$(ps ax | grep -v grep | grep SimpleHTTPServer > /dev/null && echo 0 || echo 1)
 cd /LabSetup/
 virt-manager &
 
 for i in $nodes 
 do
-	virsh list --all | grep $i | grep running > /dev/null && echo 'Domain '$i 'is currently running' && 'virsh shutdown '$i && sleep 3 || echo 'Node '$i 'down' 2>/dev/null &
+	virsh list --all | grep $i | grep running > /dev/null && echo 'Domain '$i 'is currently running' && virsh shutdown $i && sleep 3 || echo 'Node '$i 'down' 2>/dev/null &
 	sleep 3
 done
 
-for i in $networks 
+echo 'Creating networks if needed'
+for i in ${virtnets//.xml}
 do
 	virsh net-list | grep $i > /dev/null && virsh net-destroy $i || echo 'Network '$i' not found'
 	sleep 3
-	echo 'Creating Networks'
-	virsh net-list | grep $i > /dev/null && echo 'Error Destroying '$i || virsh net-create /LabSetup/virtnet/$i.xml
-	sleep 3
+	virsh net-list | grep $i > /dev/null && echo 'network '$i' found' || virsh net-create /LabSetup/virtnet/$i.xml
+	sleep 10 
 done
 
 echo 'Checking Virtual Machine status and starting VMs'
@@ -50,5 +50,3 @@ do
 	sleep 3 	
 done
 systemctl restart fence_virtd
-
-
